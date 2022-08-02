9d60d765f2893ea90b76e65d6335f115f3c3f327
Nick Bennett <nick@nickbenn.com>
2022-08-02 12:02:48 -0600

    Lab 8.2 (method refs) & 7.1 (annotations).

--- Lab08.1/test/com/javatunes/thread/RepeatingPrinterTest.java
+++ Lab08.1/test/com/javatunes/thread/RepeatingPrinterTest.java
@@ -10,25 +10,33 @@ package com.javatunes.thread;
 
 public class RepeatingPrinterTest {
 
+  static String printerName = "block-lambda";
+
+
   public static void main(String[] args) {
+
     Thread thd1 = new Thread(new RepeatingPrinter());
     thd1.start();
 
     // DONE: initialize the 'printer' reference variable with a block lambda
     //  hint: you can copy / paste the *contents* of RepeatingPrinter's run() method
     //  note: change the sysout to show "block-lambda"
-    Runnable printer = () -> {
-      while (true) {
-        System.out.println("block-lambda");
-        try {
-          Thread.sleep(500);
-        } catch (InterruptedException e) {
-        }
-      }
-    };
+    Runnable printer = RepeatingPrinterTest::repeatFast;
 
     // DONE: create another Thread, passing in the 'printer' reference as its Runnable - then start it
     Thread thd2 = new Thread(printer);
     thd2.start();
+
+    System.out.println("Main method done!");
+  }
+
+  private static void repeatFast() {
+    while (true) {
+      System.out.println(printerName);
+      try {
+        Thread.sleep(500);
+      } catch (InterruptedException e) {
+      }
+    }
   }
 }
